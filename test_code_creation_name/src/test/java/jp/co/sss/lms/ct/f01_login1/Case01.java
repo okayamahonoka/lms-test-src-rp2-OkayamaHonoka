d1c1075fe229879bdbb54e82a4e4c777fdb2f67b
okayama.honoka <okayama.honoka@192.168.0.10>
2026-09-18 16:27:11 +0900

    fix:岡山 Case01、Case2のテストコードの追記
    テストコードをTODO部分へ記述

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -35,7 +36,16 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//指定のURLに遷移する
+		goTo("http://localhost:8080/lms");
+
+		//画面遷移が行われたかどうかの確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,34 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//指定のURLに遷移する
+		goTo("http://localhost:8080/lms");
+
+		//画面遷移が行われたかどうかの確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
-	}
+		//DBに登録されていないログインIDとパスワードを入力
+		webDriver.findElement(By.name("loginId")).sendKeys("not_exist_user");
+		webDriver.findElement(By.name("password")).sendKeys("ItTest2025");
 
+		//ログインボタンの押下
+		webDriver.findElement(By.xpath("//input[@value='ログイン']")).click();
+
+		//タイトルの変更がなく、画面遷移が行われていないかの確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
+	}
 }

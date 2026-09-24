9f3f5e8efa13c4af916ff8cbf280e92c6189e91c
okayama.honoka <okayama.honoka@192.168.0.4>
2026-09-24 14:21:28 +0900

    fix:岡山 Case.03 追記
    Casa03に取り掛かり始めたため。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +37,46 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//指定のURLに遷移する
+		goTo("http://localhost:8080/lms");
+
+		//画面遷移が行われたかどうかの確認
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		//入力項目のクリアと各項目の入力(ログインID)
+		var loginIdInput = webDriver.findElement(By.name("loginId"));
+		loginIdInput.clear();
+		loginIdInput.sendKeys("StudentAA01");
+
+		//入力項目のクリアと各項目の入力(ログインID)
+		var passwordInput = webDriver.findElement(By.name("password"));
+		passwordInput.clear();
+		passwordInput.sendKeys("Studenta01");
+
+		//ログインボタンの押下
+		webDriver.findElement(By.xpath("//input[@value='ログイン']")).click();
+
+		//ページ遷移を待機する
+		org.openqa.selenium.support.ui.WebDriverWait wait = new org.openqa.selenium.support.ui.WebDriverWait(webDriver,
+				java.time.Duration.ofSeconds(10));
+		wait.until(org.openqa.selenium.support.ui.ExpectedConditions.titleIs("コース詳細 | LMS"));
+
+		//画面遷移後のタイトルの確認
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		//エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
